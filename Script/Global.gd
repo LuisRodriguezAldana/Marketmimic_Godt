018b7022dcd95829ffc084abff22f55953203c5a
Luis Rodriguez <nada>
2024-05-08 10:03:17 -0500

    Se modifica el editor y se agrega el salto

--- Script/Global.gd
+++ Script/Global.gd
@@ -1,7 +1,8 @@
 extends Node
-var axis : Vector2
+var axis : Vector3
 
-func get_axis()-> Vector2:
+func get_axis()-> Vector3:
 	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	axis.y = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
+	axis.z = int(Input.is_action_pressed("ui_jump"))
 	return axis.normalized()
--- Script/Player.gd
+++ Script/Player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody3D
-
-
 @export var Velocidad : float
-
+@export var jump: float
+var gravity: float = 9.8
 func _physics_process(delta):
 	motion_contrl()
 	
 func motion_contrl():
 	velocity.x = Test_1.get_axis().x * Velocidad
 	velocity.z = Test_1.get_axis().y * -Velocidad
+	velocity.y = Test_1.get_axis().z * jump
 	move_and_slide()
